c335d0c9e816c7c32a0833d4386702eac648aeb9
Vision Systems GmbH <contact@visionsystems.de>
2016-10-10 15:37:56 +0200

    Add mtd-utils

--- vsdeb.sh
+++ vsdeb.sh
@@ -27,7 +27,7 @@ echo baltos > /etc/hostname
 apt-get update
 
 # install core packages
-yes "Y" | apt-get install mc dstat lsof whois tmux vim usbutils psmisc policykit-1 bzip2 libconfig9 minicom rcconf
+yes "Y" | apt-get install mc dstat lsof whois tmux vim usbutils psmisc policykit-1 bzip2 libconfig9 minicom rcconf mtd-utils
 
 # install network packages
 yes "Y" | apt-get install openvpn can-utils openssh-server modemmanager iw wpasupplicant hostapd ethtool ser2net telnet telnetd libsocketcan2 nuttcp ppp ntp ntpdate socat bridge-utils
