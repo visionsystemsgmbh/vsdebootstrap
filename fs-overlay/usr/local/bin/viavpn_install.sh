ed8a5a954eeb5648d8cc1de25a92cb3f22ad6bab
Falco Hyfing <falco.hyfing@vscom.de>
2016-12-12 14:26:40 +0000

    viavpn-install: handle viavpn-bundle.zip

--- fs-overlay/usr/local/bin/viavpn_install.sh
+++ fs-overlay/usr/local/bin/viavpn_install.sh
@@ -5,22 +5,30 @@ sernum=`onrisctool -s | awk -F" " '/Serial/ {print $3}'`
 viavpn_pkg=viavpn-$sernum.zip
 
 if [ ! -e /mnt/$viavpn_pkg ]; then
-	clear
-	echo "No viaVPN data file for device with serial number $sernum found"
-	exit 1
+	if [ ! -e /mnt/viavpn-bundle.zip ]; then
+		clear
+		echo "No viaVPN data file for device with serial number $sernum found"
+		exit 1
+	else
+		unzip /mnt/viavpn-bundle.zip $viavpn_pkg -d /tmp/vscom
+		if [ ! -e /tmp/vscom/$viavpn_pkg ]; then
+			clear
+			echo "No viaVPN data file for device with serial number $sernum found"
+			exit 1
+		fi
+	fi
 fi
 
 echo Extracting files
 
 mkdir -p /tmp/vscom
-unzip -o /mnt/$viavpn_pkg -d /tmp/vscom
+unzip /mnt/$viavpn_pkg -d /tmp/vscom
 
 echo Installing certificates
 mkdir -p /opt/viavpn
-unzip -o /tmp/vscom/viavpn-data.zip -d /opt/viavpn
+unzip /tmp/vscom/viavpn-data.zip -d /opt/viavpn/data
 
 echo Installing required Debian packages
+apt update
 dpkg -i /tmp/vscom/*.deb
 yes "Y" | apt-get install -f
-
-echo Installation completed
